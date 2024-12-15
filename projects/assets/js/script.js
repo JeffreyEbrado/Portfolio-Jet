74dbbc22b1482dde8bba8bf4b3c1deb8c34bb47f
Jeffrey Ebrado <145235529+JeffreyEbrado@users.noreply.github.com>
2024-12-15 10:38:02 -1100

    Add files via upload

--- projects/assets/js/script.js
+++ projects/assets/js/script.js
@@ -21,3 +21,35 @@ function dlResume() {
 
     document.body.removeChild(link);
 }
+
+//animation
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-links a'); 
+    const panels = document.querySelectorAll('.panel'); 
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+            const targetId = link.getAttribute('href').substring(1); 
+            const targetPanel = document.getElementById(targetId);
+
+            // Scroll to the target section
+            targetPanel.scrollIntoView({ behavior: 'smooth' });
+
+            // Reset fade-in animations
+            resetFadeInAnimations(targetPanel);
+        });
+    });
+
+    function resetFadeInAnimations(panel) {
+       
+        const fadeInElements = panel.querySelectorAll('.fade-in-above, .fade-in-left, .fade-in-right, .fade-in-below');
+        fadeInElements.forEach(element => {
+           
+            element.classList.remove('fade-in-above', 'fade-in-left', 'fade-in-right', 'fade-in-below');
+           
+            void element.offsetWidth; 
+            element.classList.add('fade-in-above', 'fade-in-left', 'fade-in-right', 'fade-in-below');
+        });
+    }
+});
